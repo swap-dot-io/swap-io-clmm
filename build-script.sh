c9d0d6ac4f7dc5dbba9e9dc4797ecd967088d566
swap-io-dev <v@swap.io>
2025-03-13 17:32:01 +0100

    moved files around

--- build-script.sh
+++ build-script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
+# This builds a verifieable solana program. To be run within docker.
+
 cargo build-bpf --manifest-path Cargo.toml
-mkdir -p target
-mkdir target/idl
-mkdir target/types
+mkdir -p docker-target
+mkdir -p docker-target/idl
+mkdir -p docker-target/types
 anchor idl parse --file programs/amm/src/lib.rs -o target/idl/swap-io-clmm-idl.json -t target/types/swap-io-clmm-idl.ts
