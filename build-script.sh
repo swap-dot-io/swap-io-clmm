03b383f895751e069bac510d129858914bea7eef
swap-io-dev <v@swap.io>
2025-03-13 17:34:51 +0100

    fixed build script

--- build-script.sh
+++ build-script.sh
@@ -5,4 +5,4 @@ cargo build-bpf --manifest-path Cargo.toml
 mkdir -p docker-target
 mkdir -p docker-target/idl
 mkdir -p docker-target/types
-anchor idl parse --file programs/amm/src/lib.rs -o target/idl/swap-io-clmm-idl.json -t target/types/swap-io-clmm-idl.ts
+anchor idl parse --file programs/amm/src/lib.rs -o docker-target/idl/swap-io-clmm-idl.json -t docker-target/types/swap-io-clmm-idl.ts
